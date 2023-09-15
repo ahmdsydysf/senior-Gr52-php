23c8675b2d2d49730c5b19d43704a99468ec7e07
Ah-Sy-Yf <a.sayed.xc@gmail.com>
2023-09-15 20:07:37 +0300

    finish session 02

--- index.php
+++ index.php
@@ -1,1 +1,147 @@
-lec 2
+<?php
+
+// this is single line comment
+#  this is single line comment
+
+/*
+z
+z
+z
+z
+*/
+
+/**
+ * docBlock
+ *
+ *
+ *
+ *
+ */
+
+
+// $lastName = 'sayed';
+
+// //  lossely typed lang
+
+// $x = 1 ;
+// $x = 2 ;
+// $y = &$x ; // 1
+// $x;
+
+// $x = 100;
+
+
+// $x = 10 ;
+// $firstName = 'ahmed';
+// // ahmed  == Ahmed
+
+// // define('POST');
+// // POST = 3306 ;
+
+// function sum()
+// {
+//     $firstname = 'omaar';
+
+//     echo $firstname ;
+//     echo POST ;
+// }
+
+// sum();
+// // echo POST ;
+
+// echo $firstname ;
+// // const
+
+// $x = 'ahmed';
+// echo $x ;
+
+// function test()
+// {
+//     global $x ;
+//     echo '<br>';
+//     echo $x;
+// }
+
+// echo $x ;
+
+
+// test();// $x = 'ahmed sayed
+
+// echo $x ;
+
+
+// const DATABASE = 'Store';
+// define('POST', 3306);
+
+// echo DATABASE;
+
+
+// echo PHP_VERSION;
+// echo __LINE__;
+// echo __LINE__;
+// echo __LINE__;
+
+// Variable Variable
+
+// $x = 'name';
+
+// $$x = 'ahmed';
+
+// echo "my result is :: $$x";
+// echo "my result is :: ${$x}";
+// echo "my result is :: {$$x}";
+// echo "my result is :: " . $$x;
+
+// $firstname = 'ahmed';
+// $age = 20 ;
+// $salary = 20.50 ;
+// // boolean   bool
+// $internet_connected = true ;
+// $is_admin = false ;
+// echo $is_admin ;
+// var_dump($internet_connected, $is_admin);
+// // var_dump(5);
+
+// var_dump($firstname, $age, $salary);
+
+// print_r();
+
+// falsy => 0  ''  null -0 '0' false 0.0  -0.0 []
+// var_dump(0, null);
+
+
+$x = false;
+
+// if($x) {
+
+//     echo "yaaala";
+
+// }
+
+var_dump(gettype($x));
+
+echo PHP_INT_MAX;
+echo PHP_INT_MIN;
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
+
+<?php
+// print_r
+
+// string data type
+$x = 'ahmed';
+echo $x , gettype($x);
+var_dump($x);
+
+# integer data type
