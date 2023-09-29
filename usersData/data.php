8659439974da2065d8fa45de04288cb7e64beb54
Ah-Sy-Yf <a.sayed.xc@gmail.com>
2023-09-29 19:37:10 +0300

    update task

---
+++ usersData/data.php
@@ -0,0 +1,23 @@
+<?php
+
+define('config', 'aa');
+$countryList = [
+  'CA'        => 'Canada',
+  'US'        => 'United States'];
+
+$stateList['CA'] = [
+  'AB'        => 'Alberta',
+  'BC'        => 'British Columbia',
+  'AB'        => 'Alberta',
+  'BC'        => 'British Columbia',
+  'MB'        => 'Manitoba',
+  'NB'        => 'New Brunswick',
+  'NL'        => 'Newfoundland/Labrador',
+  'NS'        => 'Nova Scotia',
+  'NT'        => 'Northwest Territories',
+  'NU'        => 'Nunavut',
+  'ON'        => 'Ontario',
+  'PE'        => 'Prince Edward Island',
+  'QC'        => 'Quebec',
+  'SK'        => 'Saskatchewan',
+  'YT'        => 'Yukon'];
