6d16a059bcdab668d52921a417605dc0f675a019
Ah-Sy-Yf <a.sayed.xc@gmail.com>
2023-10-13 16:54:25 +0300

    part-2

--- superG.php
+++ superG.php
@@ -8,12 +8,23 @@
 
 var_dump($_GET);
 var_dump($_POST);
-$username = $_POST['uname'];
-$password = $_POST['upw'];
-$email = $_POST['email'];
+
+
+// $data = [
+//           'uname'  =>  'Ruby Rosario' ,
+//           'upw'   =>  'Nostrum quia aliquam' ,
+//           'email' =>  'ryqumomu@mailinator.com',
+//           'msg'   =>  'At dolore id quia h'
+// ];
+// echo $data['msg'];
+$un = $_POST['uname'];
+$pw = $_POST['upw'];
+$em = $_POST['email'];
 $msg = $_POST['msg'];
 
-echo $username , $password , $email , $msg ;
+echo $un , $pw , $em , $msg ;
+
+echo $_POST['uname'] , $_POST['upw'] , $_POST['email'] ,$_POST['msg'];
 ?>
 
 <!DOCTYPE html>
@@ -30,10 +41,36 @@ echo $username , $password , $email , $msg ;
 <form  action="" id="loginForm">  -->
 <form method="post" action="" id="loginForm"> 
   <input type="text" name="uname" >username
-  <input type="text" name="upw" value="">password
-  <input type="text" name="email" value="">email
-  <input type="text" name="msg" value="">message
+  <!-- $_GET['uname'] -->
 
+  <input type="text" name="upw" value="">password
+  <!-- $_GET['upw'] -->
+
+  <input type="text" name="email" value="">email
+  <!-- $_GET['email'] -->
+
+  <input type="text" name="msg" value="">message
+  <!-- $_GET['msg'] -->
+
+
+<!-- <textarea name="" id="" cols="30" rows="10"></textarea>
+<input type="checkbox" name="" id="">
+<input type="checkbox" name="" id="">
+<input type="checkbox" name="" id="">
+
+<input type="radio" name="" id="">
+<input type="radio" name="" id="">
+
+<input type="number" name="" id="">
+<input type="color" name="" id="">
+<input type="file" name="" id="">
+
+<select name="" id="">
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+  <option value=""></option>
+</select> -->
   <button form="loginForm" >login</button>
 </form>
 </body>
