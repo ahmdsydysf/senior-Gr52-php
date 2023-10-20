d23d7fbdcea10c80fa264b826567a9f8966f8e89
Ah-Sy-Yf <a.sayed.xc@gmail.com>
2023-10-20 18:31:48 +0300

    update - 4

--- inputs_ty.php
+++ inputs_ty.php
@@ -14,8 +14,63 @@ var_dump($_GET);
 var_dump($_POST);
 
 var_dump($_FILES);
+// var_dump($_SERVER);
+
+$all_errors = [];
+
+$allowed_ext = ['png' , 'jpg' , 'jpeg'];
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if(isset($_FILES['my_image'])) {
+        if($_FILES['my_image']['error'] != 4) {
+
+
+            // $img_name =  $_FILES['my_image']['name'];
+            // $img_tmp =  $_FILES['my_image']['tmp_name'];
+            // $img_size =  $_FILES['my_image']['size'];
+
+            $my_img = $_FILES['my_image'];// 6 attr
+            $img_name = uniqid() . $my_img['name']; //65328677a.pdf412bwallpap5.png5555erflare.com_wallpaper(8).jpg
+            $img_tmp = $my_img['tmp_name'];
+            $img_size = $my_img['size'] ;
+
+            $img_ext = explode('.', $img_name);//[65328677a ,pdf412bwallpap5 , png5555erflare , com_wallpaper(8) , jpg ]
+            $img_f_ext = end($img_ext);// jpg
+            $ext = strtolower($img_f_ext);
+
+            if($img_size < 2097152) {
+                if($_FILES['my_image']['type'] == 'image/jpeg') {
+
+                    if(in_array($ext, $allowed_ext)) {
+                        move_uploaded_file($img_tmp, 'uploads/profiles/' . $img_name);
+                    } else {
+                        $all_errors['f_ext'] = 'plz upload file type jpg png jpeg';
+                    }
+                }
+
+            } else {
+                $all_errors['f_size'] = 'plz upload file max 2 miga';
+            }
+
+
+        } else {
+            $all_errors['f_exist'] = 'plz choose a file';
+        }
+    }
+}
+
+
 
 ?>
+<?php if(! empty($all_errors)) : ?>
+  <?php foreach($all_errors as $error) : ?>
+
+    <div class="alert alert-info">
+      <?= $error ?>
+    </div>
+
+  <?php endforeach ?>
+<?php endif ?>
   <form method="post" enctype="multipart/form-data">
 
     <!-- <input type="text" name='un'>
